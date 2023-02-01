242ebd019b118109b3d6cc619edc3c2abd5f4074
CamilaRodrigues <camilarodriguescrs13@gmail.com>
2023-02-01 19:35:26 -0300

    versão com o html e css

---
+++ script.js
@@ -0,0 +1,27 @@
+//criar a lista vazia dos ultimos pedidos
+const ultimosPedidos = []
+
+function imagemEscolhidaPeloUsaruario () {
+   const promise = axios.post (url);
+}
+
+//fazer o requerimento para o modelo escolhido pelo usuario
+
+//adicionar o modelo escolhido na lista ultimosPedidos
+
+
+
+// definir a função que irá adicionar os pedidos que vier do navegador
+function ultimoModelo () {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/shirts-api/shirts");
+    if (promise.then (ultimoPedidoChegou)) {
+        let pedidoTemplate = `
+            <li>
+                <img src="${ultimosPedidos[i]}">
+                <p class="criador"></p>
+            </li>
+        `
+    }
+}
+
+//exibir a lista atualizada dos ultimos pedidos feitos
