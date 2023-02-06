84ca7f904c0d1d96db3d3173ba5f3cfa940090c3
CamilaRodrigues <camilarodriguescrs13@gmail.com>
2023-02-06 10:12:33 -0300

    versão com a seleção de opçoes

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ let modelos = [];
 
 function exibirModelos () {
     listaDeModelos = document.querySelector('.ultimosPedidos');
-
-    listaDeModelos = '';
-
+    
     for (let index = 0; index < modelos.length; index++) {
         let template = `
             <li>
@@ -28,54 +26,100 @@ pegarModeloEscolhido();
 exibirModelos();
 
 function selecionarOpcao(elemento) {
-    //console.log(elemento, elemento.parentNode)
 
     if (elemento.parentNode.classList.contains("opcoes-modelo")) {
-        if (elemento.classList.contains("opcao1")) {
-            modelo = "t-shirt" 
+        if (elemento.classList.contains("opcao1") ) {
+            modelo = "t-shirt";
+            
         } 
         if (elemento.classList.contains("opcao2")) {
-            modelo =  "top-tank"
+            modelo =  "top-tank";
+            
         }
         if (elemento.classList.contains("opcao3")) {
-            modelo =  "long"
+            modelo =  "long";
+            
         }
-    } 
-
+        
     if (elemento.parentNode.classList.contains("opcoes-gola")) {
         if (elemento.classList.contains("opcao1")) {
-            gola = "v-neck" 
+            gola = "v-neck";
+           
         } 
         if (elemento.classList.contains("opcao2")) {
-            gola =  "round"
+            gola =  "round";
+            
         }
         if (elemento.classList.contains("opcao3")) {
-            gola =  "polo"
+            gola =  "polo";
+            
         }
+        console.log(elemento)
     } 
 
     if (elemento.parentNode.classList.contains("opcoes-tecido")) {
         if (elemento.classList.contains("opcao1")) {
             tecido = "silk" 
+           
         } 
         if (elemento.classList.contains("opcao2")) {
             tecido =  "cotton"
+            
         }
         if (elemento.classList.contains("opcao3")) {
             tecido =  "polyester"
+           
         }
     } 
-
-    console.log("modelo: ",modelo)
-    console.log("gola: ",gola)
-    console.log("tecido: ",tecido)
-
+    
+    
+    
     //chamar a função de verifiacr o botão aqui!!!
 }
+}
+//verificar se o elemento esta com a classe selecionado aplicada
+//desmarcar essa classe ao clicar em outra opçao
+function modeloSelecionado(elemento){
+    const modeloAnterior = document.querySelector('.modelo .selecionado');
+
+    if (modeloAnterior !== null) {
+        modeloAnterior.classList.remove('selecionado');
+
+    }
+    elemento.classList.add('selecionado');
+
+    validarBotaoConfirmarPedido ()
+}
+function golaSelecionada(elemento){
+    const golaAnterior = document.querySelector('.gola .selecionado');
+   
+    if (golaAnterior !== null) {
+        golaAnterior.classList.remove('selecionado');
+
+    }
+    elemento.classList.add('selecionado');
+    
+    validarBotaoConfirmarPedido ()
+}
+
+function tecidoSelecionado(elemento){
+    const tecidoAnterior = document.querySelector('.tecido .selecionado');
+    
+    if (tecidoAnterior !== null) {
+        tecidoAnterior.classList.remove('selecionado');
+
+    }
+
+    elemento.classList.add('selecionado');
+  
+    validarBotaoConfirmarPedido ()
+} 
+
 
 function enviarPedido(){
-    const input = document.querySelector('input')
-    const urlDaImagem = input.value
+    const input = document.querySelector('input');
+    const urlDaImagem = input.value;
+    console.log (input)
 
     const body = {
         model: modelo,
@@ -106,11 +150,9 @@ function pegarModeloEscolhido() {
     promise.then(resChegaram);
     
     promise.catch((err) => {
+        alert("Ocorreu um erro inesperado no servidor!")
         console.log("Erro ao receber os modelos", err)
-    });
-    
-  
-    
+    });    
 }
 
 function resChegaram(res) {
@@ -121,4 +163,22 @@ function resChegaram(res) {
 }
 
 
+function validarBotaoConfirmarPedido (botao) {
+   //verificar se o modelo está vazio
+    if (modelo !== undefined) {
+     // verificar se a gola está vazia
+        if (gola !== undefined){
+    //verificar se o tecido está vazio
+             if (gola !== undefined){
+    //verificar se a url está vazia
+                if (input !== undefined) {
+                    const confirmarBotao = document.querySelector('.confirmar');
+
+                    confirmarBotao.classList.add('confirmarPedido');
+                }
+            }
+        }
+    }
+    console.log(botao)
+}
 
